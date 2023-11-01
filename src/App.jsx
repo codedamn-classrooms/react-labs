2796c7484082a281d01a9542937ac904bdd07add
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 13:22:24 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
+const App = () => {
+  // Edit this component
+  return (
+    <>
+      <textarea data-testid='textarea-id'/>
+      <h1 data-testid='output-id'></h1>
+    </>
+  )
 }
+
+export default App

769bbc6c6d6e41d8c2ea73b62fd9bed231256af1
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 10:42:02 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
-}
+export const useGeolocation = () => {
+  // Write the body of your hook here
+  return {};
+};
+
+const App = () => {
+  const { coords, error } = useGeolocation();
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      <p>Latitude: {coords?.latitude}</p>
+      <p>Longitude: {coords?.longitude}</p>
+    </div>
+  );
+};
+
+export default App;

27bd8ed2470e18be09e6245d0f16c871cca461cb
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 13:21:09 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
+export const useTheme = () => {
+  // Write your hook logic here
+  return {}
 }
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme();
+  return (
+    <div 
+      style={{ 
+        height: '100vh',
+        transition: '0.3s ease-in',
+        backgroundColor: theme === 'light' ? 'white' : 'black'
+      }}
+    >
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+}
+
+export default App;

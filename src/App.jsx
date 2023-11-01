03e72ccd209cdd2caa80820417e7ac6d9f981d36
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 10:47:33 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
-}
+export const useTodoList = () => {
+  // Write the body of your hook here
+  return {};
+};
+
+const App = () => {
+  const { todos, addTodo, removeTodo, toggleTodo } = useTodoList();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(e.target.elements.todoText.value);
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="todoText" />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+            <button onClick={() => toggleTodo(todo.id)}>
+              {todo.completed ? 'Incomplete' : 'Complete'}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;

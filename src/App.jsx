87df1dbafd139307198517e0504df6dd58ebe120
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 10:44:20 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
+export const useWindowDimensions = () => {
+  // Write your hook here
+  return {};
 }
+
+const App = () => {
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <div>
+      <h2>Window Dimensions:</h2>
+      <p>Width: {width}</p>
+      <p>Height: {height}</p>
+    </div>
+  );
+};
+
+export default App;

00128294f357df81ea2ba7f2dc6b04f67231aafb
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 11:04:50 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
+const App = () => {
+  // Edit this component
+  return (
+    <div>
+      <input data-testid="input-id" />
+      <button data-testid="button-id">Add a todo</button>
+      <ul data-testid="ul-id">
+        {/* Return todos here */}
+      </ul>
+    </div>
+  )
 }
+
+export default App

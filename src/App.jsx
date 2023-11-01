88a1aa52697139463274da8b26d6c9f9bcfa1fce
Mehul Mohan <mehulmpt@gmail.com>
2023-11-01 13:25:07 +0000

    Add files via script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
-	return (
-		<>
-			<h1>Hello there!</h1>
-		</>
-	);
+import React, { useState } from 'react'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2 data-testid="count-id" >Count: {count}</h2>
+      <button data-testid="inc-id" onClick={() => setCount(count + 1)}>+</button>
+      <button data-testid="dec-id" onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  )
 }
+
+export default Counter

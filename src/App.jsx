c286a6138d0b8f9904405dc4911601c0fe89056c
Pranav <pranav.techiegeek@gmail.com>
2023-11-27 19:18:26 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 export default function App() {
+	// You can make use of this object to construct the select elements and the conversion
+	const conversionRates = {
+		centimeters: 1,
+		inches: 2.54,
+		feet: 30.48,
+		meters: 100,
+		kilometers: 100000,
+		miles: 160934.4,
+	};
+
 	return (
 		<>
 			<h1>Hello there!</h1>
